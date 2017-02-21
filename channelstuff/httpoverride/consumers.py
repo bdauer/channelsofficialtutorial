5e51274adb8e4359fd7b9419e7a90a722580619b
bdauer <dauer.benjamin@gmail.com>
2017-02-21 10:05:46 -0500

    Add support for sessions

--- channelstuff/httpoverride/consumers.py
+++ channelstuff/httpoverride/consumers.py
@@ -1,22 +1,32 @@
 from django.http import HttpResponse
 from channels.handler import AsgiHandler
 from channels import Group
+from channels.sessions import channel_session
 
 # Connected to websocket.connect
-def ws_add(message):
+@channel_session
+def ws_connect(message):
+    # Accept connection
     message.reply_channel.send({"accept": True})
-    Group("chat").add(message.reply_channel)
+    # Work out room name from path {ignore slashes}
+    room = message.content['path'].strip("/")
+    # Save room in session and add us to the group
+    Group("chat-%s" % room).add(message.reply_channel)
 
+# Connected to websocket.receive
+@channel_session
 def ws_message(message):
     # ASGI WebSocket packet-received and send-packet message types
     # both have a "text" key for their textual data.
-    Group("chat").send({
-        "text": "[user] %s" % message.content['text'],
+    Group("chat-%s" % message.channel_session['room']).send({
+        "text": message['text'],
     })
 
 # Connected to websocket.disconnect
+@channel_session
 def ws_disconnect(message):
-    Group("chat").discard(message.reply_channel)
+    Group("chat-%s" % message.channel_session['room']).discard(
+                                                        message.reply_channel)
 
 
 """
